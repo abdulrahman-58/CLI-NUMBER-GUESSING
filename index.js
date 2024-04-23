31bb332f01813ce5d2bd66767a4765ef9e8e7174
abdulrahman <abdulrahmanazam58@gmail.com>
2024-04-23 13:07:48 -0700

    updated

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ import inquirer from "inquirer";
 // 1) Computer will generate a random number
 // 2) User input for guessing number
 // 3) Compare user input with computer generated number and show result
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-const answers = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
+const target = Math.floor(Math.random() * 10 + 1);
+let guess = await inquirer.prompt([{
+        name: "answer",
         type: "number",
-        message: "Please guess a number between 1-6: ",
-    },
-]);
-if (answers.userGuessedNumber === randomNumber) {
-    console.log("Congtratulations! you guessed right number.");
+        message: "Please enter a number between 1-10 :"
+    }]);
+if (guess.answer < target) {
+    console.log("Your guess is too low.");
+}
+else if (guess.answer > target) {
+    console.log("Your guess is too high");
 }
 else {
-    console.log("You guessed a wrong number");
+    console.log("You guessed correctly!");
 }
--- index.ts
+++ index.ts
@@ -5,16 +5,17 @@ import inquirer from "inquirer";
 // 2) User input for guessing number
 // 3) Compare user input with computer generated number and show result
 
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-const answers = await inquirer.prompt([
-  {
-    name: "userGuessedNumber",
-    type: "number",
-    message: "Please guess a number between 1-6: ",
-  },
-]);
-if (answers.userGuessedNumber === randomNumber) {
-  console.log("Congtratulations! you guessed right number.");
+const target: number = Math.floor(Math.random() * 10 +1);
+let guess = await inquirer.prompt([{
+    name: "answer",
+    type : "number",
+    message: "Please enter a number between 1-10 :"
+}])
+if (guess.answer < target){
+    console.log("Your guess is too low.");
+}  else if (guess.answer > target){
+    console.log("Your guess is too high");
 } else {
-  console.log("You guessed a wrong number");
+    console.log("You guessed correctly!");
 }
+
